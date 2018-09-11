d2ee8f58ad8527c9e25c2dca5a4ff19e7766693d
Dmitry <kuzin_kov@ukr.net>
2018-09-11 20:59:06 +0300

    v1

---
+++ js/script.js
@@ -0,0 +1,20 @@
+$(function() {
+    $(window).scroll(function() {
+        if($(this).scrollTop() != 0) {
+            $('#toTop').fadeIn();
+        } else {
+            $('#toTop').fadeOut();
+        }
+    });
+    $('#toTop').click(function() {
+        $('body,html').animate({scrollTop:0},800);
+    });
+});
+
+
+$(window).on('load', function () {
+    var $preloader = $('#p_prldr'),
+        $svg_anm   = $preloader.find('.svg_anm');
+    $svg_anm.fadeOut();
+    $preloader.delay(500).fadeOut('slow');
+});
